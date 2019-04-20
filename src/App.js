ce2c79c51bbc990aec12fa3676be0745093f6cd9
Orhan Hajderovic <orhanhajderovic@orhans-mbp-2.mynet>
2019-04-20 10:58:37 +0200

    redirect to /addbox when we hit root url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { HashRouter, Route, Switch } from 'react-router-dom'
+import { HashRouter, Route, Switch, Redirect } from 'react-router-dom'
 import routes from './routes';
 
 import './App.scss';
@@ -11,11 +11,23 @@ class App extends Component {
       <HashRouter>
         <Switch>
           <div className="App">
-          {routes &&
-            routes.map((route, key) => {
-              const { component, path, exact } = route;
-              return <Route key={key} exact={exact} path={path} component={component} />
-            })}
+            {routes &&
+              routes.map((route, key) => {
+                const { component, path, exact } = route;
+                return (
+                  <Route
+                    key={key} 
+                    exact={exact} 
+                    path={path} 
+                    component={component}
+                    children={({ location }) => {
+                      if (location.pathname === '/') {
+                        return <Redirect to="/addbox" />
+                      }
+                      return null;
+                    }} />
+                );
+              })}
           </div>
         </Switch>
       </HashRouter>
